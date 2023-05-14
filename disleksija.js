6a65cce10471fc7b11fedd6170f9f378e8ba574a
majamihelic <maja.mihelic1@outlook.com>
2023-05-14 11:55:28 +0200

    konc

--- disleksija.js
+++ disleksija.js
@@ -1,6 +1,6 @@
 console.log("evooooyeaaa")
 
-document.body.innerHTML = String(document.body.innerHTML);
+let celobesedilo = String(document.body.innerHTML);
 
 function bionic(str){
     let stri = str.replace(/<\/?[^>]+(>|$)/g, "");
@@ -24,10 +24,12 @@ function nobionic(str){
 
 chrome.runtime.onMessage.addListener(
     function(request) {
+        let mesic = request.message.split("_");
+
         //spremeni font
         if(request.message === "comicsansstart" ) {
             for(body of document.getElementsByTagName("body")) {
-               // div.classList.remove("") //more removat prejsn dodan class!!!!
+                // div.classList.remove("") //more removat prejsn dodan class!!!!
                 body.classList.add("comicsans")
             }
             for(span of document.getElementsByTagName("span")) {
@@ -35,10 +37,126 @@ chrome.runtime.onMessage.addListener(
                 span.classList.add("comicsans")
             }
         }
+        else if(request.message === "opendyslexic" ) {
+            alert("opendys");
+            for(body of document.getElementsByTagName("body")) {
+                // div.classList.remove("comicsans") //more removat prejsn dodan class!!!!
+                body.classList.add("dyslexie")
+            }
+            for(span of document.getElementsByTagName("span")) {
+                //div.classList.remove("comicsans") //more removat prejsn dodan class!!!!
+                span.classList.add("dyslexie")
+            }
+        }
+
+        //font size
+        else if (mesic[0] === "ss") {
+            document.body.style.fontSize = mesic[1] + "px";
+        }
+        //spacing
+        else if (mesic[0] === "sp") {
+            document.body.style.letterSpacing = mesic[1] + "px";
+        }
+        //line spacing
+        else if (mesic[0] === "ln") {
+            document.body.style.lineHeight = mesic[1] + "px";
+        }
+
+        else if (mesic[0] === "fc") {
+            //alert(mesic[1]);
+            document.body.style.color = mesic[1];
+        }
+
+        else if (mesic[0] === "bc") {
+            document.body.style.backgroundColor = mesic[1];
+            for (div of document.getElementsByTagName("div")) {
+                div.style.backgroundColor = mesic[1];
+            }
+            for (span of document.getElementsByTagName("span")) {
+                span.style.backgroundColor = mesic[1];
+            }
+            for (p of document.getElementsByTagName("p")) {
+                p.style.backgroundColor = mesic[1];
+            }
+            //alert(mesic[1]);
+        }
+
+        //ozadje
+        else if(request.message === "ozadjekrem" ) {
+            document.body.style.backgroundColor = "#f9f9d2";
+            for(p of document.getElementsByTagName("p")) {
+                p.style.backgroundColor = "#f9f9d2";
+            }
+            for(div of document.getElementsByTagName("div")) {
+                div.style.backgroundColor = "#f9f9d2";
+            }
+            for(span of document.getElementsByTagName("span")) {
+                span.style.backgroundColor = "#f9f9d2";
+            }
+        }
+        else if(request.message === "ozadjebelo" ) {
+            document.body.style.backgroundColor = "white";
+            for(p of document.getElementsByTagName("p")) {
+                p.style.backgroundColor = "white";
+            }
+            for(div of document.getElementsByTagName("div")) {
+                div.style.backgroundColor = "white";
+            }
+            for(span of document.getElementsByTagName("span")) {
+                span.style.backgroundColor = "white";
+            }
+        }
+        else if(request.message === "ozadjecrno" ) {
+            document.body.style.backgroundColor = "black";
+            for(p of document.getElementsByTagName("p")) {
+                p.style.backgroundColor = "black";
+            }
+            for(div of document.getElementsByTagName("div")) {
+                div.style.backgroundColor = "black";
+            }
+            for(span of document.getElementsByTagName("span")) {
+                span.style.backgroundColor = "black";
+            }
+        }
+        else if(request.message === "fontbela" ) {
+            document.body.style.color = "white";
+            for(p of document.getElementsByTagName("p")) {
+                p.style.color = "white";
+            }
+            for(div of document.getElementsByTagName("div")) {
+                div.style.color = "white";
+            }
+            for(span of document.getElementsByTagName("span")) {
+                span.style.color = "white";
+            }
+        }
+        else if(request.message === "fontcrna" ) {
+            //document.body.style.color = "black";
+            for(p of document.getElementsByTagName("p")) {
+                p.style.color = "black";
+            }
+            for(div of document.getElementsByTagName("div")) {
+                div.style.color = "black";
+            }
+            for(span of document.getElementsByTagName("span")) {
+                span.style.color = "black";
+            }
+        }
+        else if(request.message === "fonttmodra" ) {
+            document.body.style.color = "#000099";
+            for(p of document.getElementsByTagName("p")) {
+                p.style.color = "#000099";
+            }
+            for(div of document.getElementsByTagName("div")) {
+                div.style.color = "#000099";
+            }
+            for(span of document.getElementsByTagName("span")) {
+                span.style.color = "#000099";
+            }
+        }
 
         //bionic reading
         else if(request.message === "bionic") {
-            alert("bionicli");
             for(p of document.getElementsByTagName("p")) {
                 p.innerHTML = bionic(p.innerHTML);             }
             for(span of document.getElementsByTagName("span")) {
@@ -46,49 +164,7 @@ chrome.runtime.onMessage.addListener(
             }
         }
         else if (request.message === "nobionic") {
-            alert("nobionic")
+            document.body.innerHTML = celobesedilo;
         }
     }
 );
-
-
-
-/*
-function bionic(str){
-    let stri = str.replace(/<\/?[^>]+(>|$)/g, "");
-    let beseda = stri.split(" ");
-    let bold = [];
-    for (let i = 0; i < beseda.length; i++) {
-        let crke = "";
-        for (let j = 0; j < beseda[i].length / 2; j++) {
-            crke += beseda[i][j];
-        }
-        let ostalo = beseda[i].slice(crke.length);
-        let koncna = "<b>" + crke + "</b>" + ostalo;
-        bold.push(koncna);
-    }
-    return bold.join(" ");
-}
-
-
-/*
-//prejme sporocilo 
-chrome.runtime.onMessage.addListener(
-    function(request) {
-        if(request.message === "comicsansstart" ) {
-            comicsans();
-        }
-    }
-);
-
-//fonti
-function comicsans(){
-    alert("comic sans")
-    for(div of document.getElementsByTagName("div")) {
-        div.classList.remove("") //more removat prejsn dodan class!!!!
-        div.classList.add("comicsans")
-    }
-}
-*/
-
-
--- script.js
+++ script.js
@@ -9,8 +9,11 @@ document.getElementById('font').addEventListener('change', function comicsansmes
             chrome.tabs.sendMessage(aktivenTab.id, {message: "comicsansstart"});
         });
     }
-    else if(document.getElementById('font').value == "hobostd") {
-        alert("hobostd");
+    else if(document.getElementById('font').value == "opendyslexic") {
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+            var aktivenTab = tabs[0];
+            chrome.tabs.sendMessage(aktivenTab.id, {message: "opendyslexic"});
+        });
     }
 });
 
@@ -30,6 +33,65 @@ document.getElementById("bionic").addEventListener("change", function bionic() {
 });
 
 
+//FONT SEM DODALA ZDAJ
+document.getElementById("belaf").addEventListener("click", function fontbela() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var aktivenTab = tabs[0];
+        document.getElementById("FR").value = 255;
+        document.getElementById("FG").value = 255;
+        document.getElementById("FB").value = 255;
+        chrome.tabs.sendMessage(aktivenTab.id, {message: "fontbela"});
+    });
+});
+document.getElementById("crnaf").addEventListener("click", function fontcrna() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var aktivenTab = tabs[0];
+        document.getElementById("FR").value = 0;
+        document.getElementById("FG").value = 0;
+        document.getElementById("FB").value = 0;
+        chrome.tabs.sendMessage(aktivenTab.id, {message: "fontcrna"});
+    });
+});
+document.getElementById("tmodraf").addEventListener("click", function fonttmodra() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var aktivenTab = tabs[0];
+        document.getElementById("FR").value = 0;
+        document.getElementById("FG").value = 0;
+        document.getElementById("FB").value = 153;
+        chrome.tabs.sendMessage(aktivenTab.id, {message: "fonttmodra"});
+    });
+});
+
+//ozadje
+document.getElementById("krem").addEventListener("click", function ozadjekrem() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var aktivenTab = tabs[0];
+        document.getElementById("BR").value = 249;
+        document.getElementById("BG").value = 249;
+        document.getElementById("BB").value = 210;
+        chrome.tabs.sendMessage(aktivenTab.id, {message: "ozadjekrem"});
+    });
+});
+
+document.getElementById("bela").addEventListener("click", function ozadjebelo() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var aktivenTab = tabs[0];
+        document.getElementById("BR").value = 255;
+        document.getElementById("BG").value = 255;
+        document.getElementById("BB").value = 255;
+        chrome.tabs.sendMessage(aktivenTab.id, {message: "ozadjebelo"});
+    });
+});
+
+document.getElementById("crna").addEventListener("click", function ozadjecrno() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var aktivenTab = tabs[0];
+        document.getElementById("BR").value = 0;
+        document.getElementById("BG").value = 0;
+        document.getElementById("BB").value = 0;
+        chrome.tabs.sendMessage(aktivenTab.id, {message: "ozadjecrno"});
+    });
+});
 
 //barva fontov in odzadja
 var Sslider = document.getElementById("size");
@@ -42,14 +104,28 @@ var BRslider = document.getElementById("BR");
 var BGslider = document.getElementById("BG");
 var BBslider = document.getElementById("BB");
 Sslider.oninput = function () {
-    document.body.style.fontSize = this.value + "px";
-    //document.body.style.color = "#" + Number(2 * this.value).toString(16) + "0000";
+    //document.body.style.fontSize = this.value + "px";
+    let ssslidervalue = "ss_" + String(Number(this.value));
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var aktivenTab = tabs[0];
+        chrome.tabs.sendMessage(aktivenTab.id, {message: ssslidervalue});
+    });
 }
 SPslider.oninput = function () {
-    document.body.style.letterSpacing = this.value + "px";
+    //document.body.style.letterSpacing = this.value + "px";
+    let spslidervalue = "sp_" + String(Number(this.value));
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var aktivenTab = tabs[0];
+        chrome.tabs.sendMessage(aktivenTab.id, {message: spslidervalue});
+    });
 }
 LNslider.oninput = function () {
-    document.body.style.lineHeight = this.value + "px";
+    //document.body.style.lineHeight = this.value + "px";
+    let lnslidervalue = "ln_" + String(Number(this.value));
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var aktivenTab = tabs[0];
+        chrome.tabs.sendMessage(aktivenTab.id, {message: lnslidervalue});
+    });
 }
 FRslider.oninput = function () {
     var R = "";
@@ -67,7 +143,11 @@ FRslider.oninput = function () {
         B = "0"
     }
     B += Number(FBslider.value).toString(16);
-    document.body.style.color = "#" + R + G + B;
+    val = "fc_#" + R + G + B;
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var aktivenTab = tabs[0];
+        chrome.tabs.sendMessage(aktivenTab.id, {message: val});
+    });
 }
 FGslider.oninput = function () {
     var R = "";
@@ -85,7 +165,11 @@ FGslider.oninput = function () {
         B = "0"
     }
     B += Number(FBslider.value).toString(16);
-    document.body.style.color = "#" + R + G + B;
+    val = "fc_#" + R + G + B;
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var aktivenTab = tabs[0];
+        chrome.tabs.sendMessage(aktivenTab.id, {message: val});
+    });
 }
 FBslider.oninput = function () {
     var R = "";
@@ -103,7 +187,11 @@ FBslider.oninput = function () {
         B = "0"
     }
     B += Number(FBslider.value).toString(16);
-    document.body.style.color = "#" + R + G + B;
+    val = "fc_#" + R + G + B;
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var aktivenTab = tabs[0];
+        chrome.tabs.sendMessage(aktivenTab.id, {message: val});
+    });
 }
 BRslider.oninput = function () {
     var R = "";
@@ -121,7 +209,11 @@ BRslider.oninput = function () {
         B = "0"
     }
     B += Number(BBslider.value).toString(16);
-    document.body.style.backgroundColor = "#" + R + G + B;
+    val = "bc_#" + R + G + B;
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var aktivenTab = tabs[0];
+        chrome.tabs.sendMessage(aktivenTab.id, {message: val});
+    });
 }
 BGslider.oninput = function () {
     var R = "";
@@ -139,7 +231,11 @@ BGslider.oninput = function () {
         B = "0"
     }
     B += Number(BBslider.value).toString(16);
-    document.body.style.backgroundColor = "#" + R + G + B;
+    val = "bc_#" + R + G + B;
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var aktivenTab = tabs[0];
+        chrome.tabs.sendMessage(aktivenTab.id, {message: val});
+    });
 }
 BBslider.oninput = function () {
     var R = "";
@@ -157,5 +253,9 @@ BBslider.oninput = function () {
         B = "0"
     }
     B += Number(BBslider.value).toString(16);
-    document.body.style.backgroundColor = "#" + R + G + B;
+    val = "bc_#" + R + G + B;
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+        var aktivenTab = tabs[0];
+        chrome.tabs.sendMessage(aktivenTab.id, {message: val});
+    });
 }
